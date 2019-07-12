6e89946a19f8d3707611eadade808b8246a69da3
VBTX <jv.svedaite@gmail.com>
2019-07-12 12:47:05 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+json = ActiveSupport::JSON.decode(File.read('db/seeds/events.json'))
+json.each do |record|
+  Event.create!(record)
+end
